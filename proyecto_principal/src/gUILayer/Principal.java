a63af1f677628801dd91feb9aa98704f8a4e48e0
Polartic <l22170607@culiacan.tecnm.mx>
2025-09-23 21:05:24 -0700

    archivos tipo dat y buscador de archivos

--- proyecto_principal/src/gUILayer/Principal.java
+++ proyecto_principal/src/gUILayer/Principal.java
@@ -1,13 +1,20 @@
 package gUILayer;
 
+import javax.swing.Box;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JToolBar;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -22,6 +29,9 @@ public class Principal extends JFrame implements ActionListener {
 	private JMenuBar menuprincipal;
 	private JMenu jmarchivo, jmherramientas, jmsubmenu;
 	private JMenuItem jmiabrir, jmiguardar, jmisalir, jmicolor, jmiop1, jmiop2;
+	private JToolBar jtbsecciones;
+	private JButton btnlibros, btnusuarios, btnsalir;
+	private JButton jblibros, jbusuarios;
 
 	public Principal(String title) {
 		super(title);
@@ -41,7 +51,8 @@ public class Principal extends JFrame implements ActionListener {
 
 	private void initcomponents(){
         menuprincipal = new JMenuBar();
-        //archivo
+		contenidoToolBar();
+        
         jmarchivo = new JMenu("Archivo");
         jmarchivo.setMnemonic('A');
         jmiabrir = new JMenuItem("Abrir", 'A');
@@ -54,9 +65,53 @@ public class Principal extends JFrame implements ActionListener {
         jmisalir = new JMenuItem("Salir", 'S');
         jmisalir.addActionListener(this);
         jmarchivo.add(jmisalir);
-
+		
         menuprincipal.add(jmarchivo);
         setJMenuBar(menuprincipal);
+    }
+
+	private void contenidoToolBar() {
+        jtbsecciones = new JToolBar();
+        btnlibros = new JButton();
+        btnlibros.addActionListener(this);
+        btnlibros.setToolTipText("Abri22r");
+        btnlibros.setBorderPainted(false);
+        btnlibros.setBackground(Color.LIGHT_GRAY);
+        btnlibros.setIcon(new ImageIcon("imagenes/icons8-open-24.png"));
+        jtbsecciones.add(btnlibros);
+        jtbsecciones.addSeparator();
+
+        btnusuarios = new JButton();
+        btnusuarios.addActionListener(this);
+        btnusuarios.setToolTipText("Guardar");
+        btnusuarios.setBorderPainted(false);
+        btnusuarios.setBackground(Color.LIGHT_GRAY);
+        btnusuarios.setIcon(new ImageIcon("imagenes/icons8-save-50.png"));
+        jtbsecciones.add(btnusuarios);
+
+        jtbsecciones.addSeparator();
+        btnusuarios = new JButton();
+        btnusuarios.addActionListener(this);
+        btnusuarios.setToolTipText("Color");
+        btnusuarios.setBorderPainted(false);
+        btnusuarios.setBackground(Color.LIGHT_GRAY);
+        btnusuarios.setIcon(new ImageIcon("imagenes/icons8-color-30.png"));
+        jtbsecciones.add(btnusuarios);
+
+        jtbsecciones.add(new JSeparator(JSeparator.VERTICAL));
+        jtbsecciones.add(Box.createHorizontalGlue());
+        btnsalir = new JButton();
+        btnsalir.addActionListener(this);
+        btnsalir.setToolTipText("Salir de la aplicacion");
+        btnsalir.setBorderPainted(false);
+        btnsalir.setBackground(Color.LIGHT_GRAY);
+        btnsalir.setIcon(new ImageIcon("imagenes/icons8-exit-50.png"));
+        jtbsecciones.add(btnsalir);
+
+		this.add(jtbsecciones, "North");
+		JButton btn1 = new JButton("Vista 1212");
+        JButton btn2 = new JButton("Vista 2");
+
     }
 
 	@Override
@@ -64,18 +119,17 @@ public class Principal extends JFrame implements ActionListener {
 		if(e.getSource() == jmiabrir) {
 			JFileChooser jfcabrir = new JFileChooser();
             jfcabrir.setDialogTitle("Abrir un archivo");
-            jfcabrir.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            FileNameExtensionFilter fnef = new FileNameExtensionFilter("Archivos de texto","txt");
-            FileNameExtensionFilter filtropdf = new FileNameExtensionFilter("Archivos PDF","pdf");
-            jfcabrir.setFileFilter(filtropdf);
-            jfcabrir.addChoosableFileFilter(fnef);
-            jfcabrir.addChoosableFileFilter(filtropdf);
+            FileNameExtensionFilter fndat = new FileNameExtensionFilter("Archivos de tipo dat","dat");
+            jfcabrir.addChoosableFileFilter(fndat);
+            jfcabrir.setFileFilter(fndat);
             if(jfcabrir.showOpenDialog(this) != JFileChooser.CANCEL_OPTION){
                 File archivo = jfcabrir.getSelectedFile();
                 System.out.println(archivo.getName());
 			}
 		}
+		//else if (e.getSource() == )
 	}
+
 }
 
 
